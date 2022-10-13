9692cd0807f252f46acf9005cca1f9cbe2a6aaab
Mikepopup <box-need007@mail.ru>
2022-10-13 21:21:01 +0800

    updated #01

--- main.go
+++ main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	"net/http"
+	
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+	fmt.Fprint(w, "<h1>Welcome to my great site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r*http.Request){
+	w.Header().Set("Content-Type", "text/html; charset=utf8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:box-need007@mail.ru\">mikepopup.com</a>.")
+}
+
+func pathHandler(w http.ResponseWriter, r * http.Request){
+	fmt.Fprint(w, r.URL.Path)
 }
 
 func main() {
 	
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on 3000...")
 	http.ListenAndServe(":3000", nil)
 }
-  
+   

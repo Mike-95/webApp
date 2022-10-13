5511c21c89191eb51ae99946939e9c42bfb09b03
Mikepopup <box-need007@mail.ru>
2022-10-13 23:33:57 +0800

    update #02

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,20 +11,29 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my great site!</h1>")
 }
 
-func contactHandler(w http.ResponseWriter, r*http.Request){
+func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:box-need007@mail.ru\">mikepopup.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r * http.Request){
-	fmt.Fprint(w, r.URL.Path)
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not foundd", http.StatusNotFound)
+		
+	}
+
 }
 
 func main() {
-	
+
 	http.HandleFunc("/", pathHandler)
 	//http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on 3000...")
 	http.ListenAndServe(":3000", nil)
 }
-   
